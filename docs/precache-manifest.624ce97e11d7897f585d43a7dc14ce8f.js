47d743805cee75c9aa415744d062dea262ebb299
Roberto Ortega <Roberto Ortega>
2020-08-31 10:51:22 -0500

    Second commit

---
+++ docs/precache-manifest.624ce97e11d7897f585d43a7dc14ce8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b5ff67812f41f2321103ffaf058f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "717cd8bd3782bc4d43cf",
+    "url": "/static/css/main.e307cf57.chunk.css"
+  },
+  {
+    "revision": "29c50caa714f6f23f7c9",
+    "url": "/static/js/2.932db0a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.932db0a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717cd8bd3782bc4d43cf",
+    "url": "/static/js/main.5f45be03.chunk.js"
+  },
+  {
+    "revision": "084dcc523ee3cefbe373",
+    "url": "/static/js/runtime-main.f4377cb7.js"
+  }
+]);
